b7c2a450588a1be4c4b396a7f4a3f89197e17102
zhu2qian1 <16220156+zhu2qian1@users.noreply.github.com>
2025-02-08 20:11:50 +0900

    fix main

--- run.py
+++ run.py
@@ -2,5 +2,9 @@ import src.gameutil as gameutil
 import src.geoapi as geoapi
 import sys
 
-pref = geoapi.GeoAPIInterface(sys.argv[1])
-gameutil.city_name_guesser(pref)
+def main():
+    pref = geoapi.GeoAPIInterface(sys.argv[1])
+    gameutil.city_name_guesser(pref)
+
+if __name__ == "__main__":
+    main()
