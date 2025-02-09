c01aa2434f1b7cc8f30d5980cd14f1fbd858e539
zhu2qian1 <16220156+zhu2qian1@users.noreply.github.com>
2025-02-09 11:41:30 +0900

    add argument check

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ import src.geoapi as geoapi
 import sys
 
 def main():
+    if (len(sys.argv) <= 1):
+        print("引数に都道府県名を指定してください。")
+        exit(1)
     pref = geoapi.GeoAPIInterface(sys.argv[1])
     gameutil.city_name_guesser(pref)
 
